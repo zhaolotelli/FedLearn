8fb8db62b6a7ae43e4d9940d680f35d16738f560
zhaolotelli <37173685+zhaolotelli@users.noreply.github.com>
2021-04-25 21:48:36 +0800

    Update Client_Data.py

--- datasets/emnist/Client_Data.py
+++ datasets/emnist/Client_Data.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 class Client_Data(object):
-  def __init__(self, dataset, id):
-    data = dataset.create_tf_dataset_for_client(id)
-    pixels = np.array([example['pixels'].numpy().reshape(-1) for example in data])
-    labels = np.array([example['label'].numpy() for example in data])
-    self.X = pixels.astype(np.float32)
-    self.y = labels.astype(np.int64)
-
-  def __len__(self):
-    return len(self.y)
+    def __init__(self, dataset, id):
+        data = dataset.create_tf_dataset_for_client(id)
+        pixels = np.array([example['pixels'].numpy().reshape(-1) for example in data])
+        labels = np.array([example['label'].numpy() for example in data])
+        self.X = pixels.astype(np.float32)
+        self.y = labels.astype(np.int64)
+    
+    def __len__(self):
+        return len(self.y)
