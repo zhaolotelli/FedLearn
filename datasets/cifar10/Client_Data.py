14157150634c6592b2a49237b2bf1c95fc49f182
zhaolotelli <37173685+zhaolotelli@users.noreply.github.com>
2021-04-26 21:11:15 +0800

    Update Client_Data.py

--- datasets/cifar10/Client_Data.py
+++ datasets/cifar10/Client_Data.py
@@ -1,6 +1,7 @@
 import torch
+import numpy as np
 import flearn.servers.server
-from datasets.cifar10.preprocess import client_idxs
+from datasets.cifar10.preprocess import client_idxs, num_clients
 
 IDs = np.arange(num_clients)
 
@@ -8,7 +9,7 @@ class Client_Data(object):
     def __init__(self, dataset, id):
         idx = client_idxs[id]
         self.X = dataset.data[idx]
-        self.y = np.array(dataset.targets)[idx]
+        self.y = np.array(dataset.targets, dtype = np.int64)[idx]
     
     # must define len() method for Client Data object
     def __len__(self):
